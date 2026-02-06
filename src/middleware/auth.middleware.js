8092d942dce7dd766bbfa0df4e02ee802c158398
Jacob Bondar <jacob.bondar@gmail.com>
2026-02-06 13:36:20 +0200

    Updated auth.middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -35,7 +35,6 @@ const protect = async (req, res, next) => {
   }
 };
 
-// Generate JWT token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE || '30d'
